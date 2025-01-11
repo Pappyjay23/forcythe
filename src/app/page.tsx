138b01195329c421f1c400ec82d00f4240d88d1f
Pappyjay23 <pjinadu02@gmail.com>
2025-01-11 17:11:18 +0100

    feat: Created the hero section and it's functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
 "use client";
 
+import HeroSection from "@/components/hero/HeroSection";
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
 import React from "react";
-import Logo from "@/assets/images/header-background.svg";
 
 const Home = () => {
 	return (
 		<div className='relative min-h-screen w-full'>
 			<Navbar />
 			<main className='mt-[110px]'>
-				<div className='relative h-[90vh]'>
-					<Image
-						src={Logo}
-						alt='Forcythe logo'
-						layout='fill'
-						objectFit='cover'
-						quality={100}
-						decoding='async'
-						className='w-full h-full object-cover'
-					/>
-				</div>
+				<HeroSection />
 			</main>
 		</div>
 	);
